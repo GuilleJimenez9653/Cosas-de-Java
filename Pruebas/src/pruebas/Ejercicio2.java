723d2bf67a2b96fd12f8fb5000b721f13907a96d
guill <jimenezguillermo1208@gmail.com>
2021-01-05 18:37:26 -0400

    Termino el segundo ejercicio(falta visto bueno del prof)

--- Pruebas/src/pruebas/Ejercicio2.java
+++ Pruebas/src/pruebas/Ejercicio2.java
@@ -1,5 +1,86 @@
 package pruebas;
 
+import java.util.*;
+
 public class Ejercicio2 {
 
+}
+
+class Capa{
+	
+	Capa siguiente = null;
+	int posicion;
+	int size;
+	public A a = new A();
+	ArrayList<A> objetos = new ArrayList<A>();
+	
+	//Funcion a resolver
+	public A get(Filtro f,Reductor r) {
+		Iterator<A> itr = objetos.iterator();
+		A menor = null;
+		while(itr.hasNext()) {
+			A aux = itr.next();
+			A aux1 = itr.next();
+			if(f.filtrar(aux) && aux!=null && aux1!=null) {
+				menor = r.reducir(aux,aux1);
+			}
+		}
+		return menor;
+	}
+	
+	public A get(int posicion) {
+		A a1 = null;
+		for(A a: objetos) {
+			if (a.getPosicion() == posicion) {
+				a1 = a;
+			}
+		}
+		return a1;
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+}
+
+class Reductor{
+	
+	public A reducir(A a1,A a2) {
+		if(a1.getEnergia() <= a2.getEnergia()) {
+			return a1;
+		}else {
+			return a2;
+		}
+	}
+}
+
+class A{
+	
+	int energia;
+	int posicion;
+
+	public int getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
+	}
+
+	public int getEnergia() {
+		return energia;
+	}
+
+	public void setEnergia(int energia) {
+		this.energia = energia;
+	}
+	
+}
+
+class Filtro{
+	
+	public boolean filtrar(A a) {
+		return (a.getEnergia()) >= 100;
+	}
 }

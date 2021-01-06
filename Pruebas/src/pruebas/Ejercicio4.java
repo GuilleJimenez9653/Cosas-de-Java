27387e1129f97524505cb1cfa934332917570adc
guill <jimenezguillermo1208@gmail.com>
2021-01-06 17:41:05 -0400

    .

--- Pruebas/src/pruebas/Ejercicio4.java
+++ Pruebas/src/pruebas/Ejercicio4.java
@@ -38,4 +38,21 @@ class Casilla_ej_4{
 class Restriccion{
 	private int suma;
 	ArrayList<Casilla_ej_4> casillas = new ArrayList<Casilla_ej_4>();
+	
+	public boolean seCumple() {
+		int suma,cont,resultado = 0;
+		cont = 0;
+		suma = 0;
+		for(Casilla_ej_4 c: casillas) {
+			if(cont == 0) {
+				resultado = c.getValor();
+			}else {
+				cont += 1;
+				if(!c.isTachada()) {
+					suma += c.getValor();
+				}
+			}
+		}
+		return suma == resultado;
+	}
 }
